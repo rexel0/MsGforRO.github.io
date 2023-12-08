818e12bfeca90b5efe328959d69ce2816bd88eec
rexel0 <152779906+rexel0@users.noreply.github.com>
2023-12-09 02:39:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const button = document.getElementById('interactiveButton');
-  button.addEventListener('click', function () {
-    alert('Hello, Sir Meow!');
-  });
+    const button = document.getElementById('holdButton');
+    button.addEventListener('click', function () {
+        changeBackgroundAndText();
+    });
 });
+
+function changeBackgroundAndText() {
+    const backgrounds = ['secondBackground.jpg', 'thirdBackground.jpg'];
+    const texts = ['Your text here', 'Another text'];
+    
+    const randomIndex = Math.floor(Math.random() * backgrounds.length);
+
+    const background = backgrounds[randomIndex];
+    const text = texts[randomIndex];
+
+    document.querySelector('.background').src = background;
+    document.querySelector('h1').innerText = text;
+}
