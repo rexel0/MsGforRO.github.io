7749ad13e5425024f5e22339ac91570be1fdc618
rexel0 <152779906+rexel0@users.noreply.github.com>
2023-12-09 02:41:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+let isButtonPressed = false;
+
 document.addEventListener('DOMContentLoaded', function () {
     const button = document.getElementById('holdButton');
     button.addEventListener('click', function () {
+        isButtonPressed = !isButtonPressed;
         changeBackgroundAndText();
     });
 });
 
 function changeBackgroundAndText() {
-    const backgrounds = ['secondBackground.jpg', 'thirdBackground.jpg'];
-    const texts = ['Your text here', 'Another text'];
-    
-    const randomIndex = Math.floor(Math.random() * backgrounds.length);
+    const backgrounds = ['waiting.jpg', 'secondBackground.jpg', 'thirdBackground.jpg'];
+    const texts = ["I'm waiting for you", 'Your text here', 'Another text'];
+
+    const randomIndex = isButtonPressed ? 0 : Math.floor(Math.random() * (backgrounds.length - 1)) + 1;
 
     const background = backgrounds[randomIndex];
     const text = texts[randomIndex];
